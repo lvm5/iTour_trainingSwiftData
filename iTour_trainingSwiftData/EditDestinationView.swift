3cb1f1b92193c2c9a61b6b3784b0cc4197b467ac
lvm5 <lvm5>
2024-07-29 14:38:37 -0300

    2024-07-29-14:38

---
+++ iTour_trainingSwiftData/EditDestinationView.swift
@@ -0,0 +1,18 @@
+//
+//  EditDestinationView.swift
+//  iTour_trainingSwiftData
+//
+//  Created by Leandro Morais on 29/07/24.
+//
+
+import SwiftUI
+
+struct EditDestinationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditDestinationView()
+}
