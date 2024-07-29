6c8509d18f8fad4b38090e0420e474ae7ddea469
lvm5 <lvm5>
2024-07-29 18:03:00 -0300

    2024-07-29-18:02 filters

---
+++ iTour_trainingSwiftData/DestinationListingView.swift
@@ -0,0 +1,18 @@
+//
+//  DestinationListingView.swift
+//  iTour_trainingSwiftData
+//
+//  Created by Leandro Morais on 29/07/24.
+//
+
+import SwiftUI
+
+struct DestinationListingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DestinationListingView()
+}
