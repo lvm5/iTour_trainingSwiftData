ea03850acb686fda8cee1092a89c7d6e2cbfd68d
lvm5 <lvm5>
2024-07-29 11:40:56 -0300

    insert SwiftData

--- iTour_trainingSwiftData/iTour_trainingSwiftDataApp.swift
+++ iTour_trainingSwiftData/iTour_trainingSwiftDataApp.swift
@@ -5,6 +5,7 @@
 //  Created by Leandro Morais on 29/07/24.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct iTour_trainingSwiftDataApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: Destination.self)
     }
 }
