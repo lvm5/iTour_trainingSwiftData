908bd2baaa65997d0eb65434c5f0d82cfd9347ea
lvm5 <lvm5>
2024-07-29 10:06:30 -0300

    Initial Commit

---
+++ iTour_trainingSwiftData/iTour_trainingSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTour_trainingSwiftDataApp.swift
+//  iTour_trainingSwiftData
+//
+//  Created by Leandro Morais on 29/07/24.
+//
+
+import SwiftUI
+
+@main
+struct iTour_trainingSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
